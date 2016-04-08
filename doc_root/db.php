583bde01fbe175caff91562ebd05ef5a28723199
Michael Tel <michael@hollyhox.com>
2016-04-07 20:43:07 -0700

    fix bug in dbtest

--- doc_root/db.php
+++ doc_root/db.php
@@ -8,13 +8,13 @@ class db
 	{
 		$this->conn = new mysqli('localhost', 'sacsos_user', 'ilike7', 'sacsos');
 		// GRANT ALL PRIVILEGES ON *.* TO `sacsos_user`@`*` identified by 'ilike7';
-		
+		//GRANT ALL PRIVILEGES ON *.* TO `sacsos_user`@`localhost` identified by 'ilike7';
 	}
 
 	function query($sql){
 		$this->errno = $this->error='';
 		
-		$result = $this->conn->query($sql, $db);
+		$result = $this->conn->query($sql);
 		if($result==false){
 			$this->errno = $this->conn->errno;
 			$this->error = $this->conn->error;
